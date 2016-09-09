1b7f48f7d543d50203e35fd2c329727b55650572
Daniel Rizzuto <danielrizzuto@icloud.com>
2016-09-09 14:49:46 -0300

    unidade de controle

--- Unidade de Controle.vhd
+++ Unidade de Controle.vhd
@@ -9,76 +9,86 @@ ENTITY unidade_controle IS
    PORT(
       clk      : IN   STD_LOGIC;
       partida  : IN   STD_LOGIC;
-      reset    : IN   STD_LOGIC;
       fim	   : IN STD_LOGIC;
-      saida    : OUT  STD_LOGIC_VECTOR(1 downto 0);
-      estados  : OUT  STD_LOGIC_VECTOR(1 downto 0);
-      pronto   : OUT STD_LOGIC;
-      partida_x  : OUT STD_LOGIC
+
+      conta   : OUT STD_LOGIC;
+      desloca   : OUT STD_LOGIC;
+      zera   : OUT STD_LOGIC;
+      registra   : OUT STD_LOGIC;
       );
 END unidade_controle;
 
-ARCHITECTURE exemplo OF unidade_controle IS
-   TYPE tipo_estado IS (s0, s1, s2,s3);
+ARCHITECTURE uc OF unidade_controle IS
+   TYPE tipo_estado IS (s0, s1, s2, s3);
    SIGNAL estado   : tipo_estado;
-   signal partidaz : std_logic;
+   SIGNAL partidaz   : STD_LOGIC;
+   SIGNAL partida_x   : STD_LOGIC;
 BEGIN
 	PROCESS (clk,partida)
 	BEGIN
-		if clk='1' and clk'event then
+		if (clk='1' and clk'event) then
 			partidaz<=partida;
 		end if;
 		partida_x <= partida and (not partidaz);
 	END PROCESS;
 
-   PROCESS (clk, reset)
+   PROCESS (clk)
    BEGIN
-   
-      IF reset = '1' THEN
-         estado <= s0;
-         
-      ELSIF (clk'EVENT AND clk = '1') THEN
+      IF (clk'EVENT AND clk = '1' AND partida_x = '1') THEN
          CASE estado IS
-            WHEN s0=>
-               IF (partida and (not partidaz))='1' THEN
+
+            WHEN s0 =>
+               IF (partida_x) = '1' THEN
                   estado <= s1;
                ELSE
                   estado <= s0;
                END IF;
                
-            WHEN s1=>
+            WHEN s1 =>
                   estado <= s2;
-               
-            WHEN s2=> 
+
+            WHEN s2 => 
 				 IF fim = '0' THEN
                   estado <= s2;
                ELSE
                   estado <= s3;
                END IF;
-           WHEN s3=> 
+
+            WHEN s3 => 
                   estado <= s0;
+
          END CASE;
       END IF;
    END PROCESS;
-   
+
    PROCESS (estado)
    BEGIN
       CASE estado IS
          WHEN s0 =>
-            saida <= "00";
-            estados <= "00";
-            pronto<='0';
+            zera <= '0';
+            registra <= '0';
+            desloca <= '0';
+            conta <= '0';
+
          WHEN s1 =>
-            saida <= "01";
-            estados <= "01";
+            zera <= '0';
+            registra <= '1';
+            desloca <= '0';
+            conta <= '0';
+
          WHEN s2 =>
-            saida <= "10";
-            estados <= "10";
+            zera <= '0';
+            registra <= '0';
+            desloca <= '0';
+            conta <= '1';
+
          WHEN s3 =>
-            saida <= "10";
-            estados <= "10";
-            pronto <='1';
+            zera <= '1';
+            registra <= '0';
+            desloca <= '0';
+            conta <= '0';
+
       END CASE;
    END PROCESS;
    
-END exemplo;-- VHDL da Unidade de Controle
+END uc;-- VHDL da Unidade de Controle
