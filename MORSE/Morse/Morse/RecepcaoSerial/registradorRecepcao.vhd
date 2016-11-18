b870b2d4e15869bed89e08898b3b6d9b0e2e5f4e
Daniel Rizzuto <danielrizzuto@icloud.com>
2016-11-18 11:00:55 -0200

    Recepcao Morse

--- MORSE/Morse/Morse/RecepcaoSerial/registradorRecepcao.vhd
+++ MORSE/Morse/Morse/RecepcaoSerial/registradorRecepcao.vhd
@@ -6,25 +6,28 @@ entity registradorRecepcao is
     port (
         clk : in  std_logic;
         reset : in  std_logic;
-        registra : in std_logic;
-        serial : in std_logic;
+        registraPonto : in std_logic;
+        registraTraco : in std_logic;
 
-        dados : out std_logic_vector(10 downto 0)
+        dados : out std_logic_vector(11 downto 0)
     );
 end entity;
 
 architecture rtl of registradorRecepcao is
-    signal REGISTRADOR : std_logic_vector(10 downto 0);
+    signal REGISTRADOR : std_logic_vector(11 downto 0);
 begin
-    process (clk, REGISTRADOR, registra)
+    process (clk, REGISTRADOR, registraPonto, registraTraco)
     begin
         if (reset = '1') then
-            dados <= "00000000000";
-            REGISTRADOR <= "00000000000";
+            dados <= "111111111111";
+            REGISTRADOR <= "111111111111";
         elsif (clk'event and clk='1') then
-            if (registra = '1') then
-                dados <= serial & REGISTRADOR(10 downto 1);
-                REGISTRADOR <= serial & REGISTRADOR(10 downto 1);
+            if (registraPonto = '1') then
+                dados <= "00" & REGISTRADOR(11 downto 2);
+                REGISTRADOR <= "00" & REGISTRADOR(11 downto 2);
+            elsif (registraTraco = '1') then
+                dados <= "01" & REGISTRADOR(11 downto 2);
+                REGISTRADOR <= "01" & REGISTRADOR(11 downto 2);
             end if;     
         end if;
     end process;
--- MORSE/Morse/Morse/RecepcaoSerial/unidadeControleRecepcao.vhd
+++ MORSE/Morse/Morse/RecepcaoSerial/unidadeControleRecepcao.vhd
@@ -5,24 +5,24 @@ library IEEE;
 entity unidadeControleRecepcao is
     port (
         clk: in  std_logic;
-        serialStartBit : in std_logic;
-        prontoConta4 : in std_logic;
-        prontoConta8 : in std_logic;
-        fim : in std_logic; 
+        bitSerial : in std_logic;
+        liga : in std_logic;
+        fimConta4 : in std_logic;
+        fimConta3 : in std_logic;
         reset: in  std_logic;
-        recebeDado: in std_logic;
         
-        pronto : out std_logic;
-        conta : out std_logic;
-        registra : out std_logic;
+        mostraDadoDisplay : out std_logic;
+        conta4 : out std_logic;
+        conta3 : out std_logic;
+        registraPonto : out std_logic;
+        registraTraco : out std_logic;
         zera : out std_logic;
-        temDado : out std_logic;
         estadoDepuracao : out std_logic_vector(2 downto 0)
     );
 end entity;
 
 architecture rtl of unidadeControleRecepcao is 
-    type tipoEstado is (s0, s1, s2, s3, s4);
+    type tipoEstado is (s0, s1, s2, s3, s4, s5, s6);
     signal estado : tipoEstado;
     
 begin
@@ -34,35 +34,44 @@ begin
                 estado <= s0;
                 estadoDepuracao <= "000";
             end if;
-            if (estado /= s0) then
+            if (estado /= s1) then
                 case estado is
                     when s0 =>
+                        if (liga = '1') then
+                            estado <= s1;
+                            estadoDepuracao <= "001";
+                        end if ;
                     when s1 =>
-                        if (prontoConta4 = '1') then
-                            estado <= s2;
-                            estadoDepuracao <= "010";
-                        end if;
                     when s2 =>
-                        if (fim = '0') then
-                            estado <= s3;
-                            estadoDepuracao <= "011";
-                        else
-                            estado <= s4;
-                            estadoDepuracao <= "100";
+                        if (fimConta4 = '1') then
+                            if (bitSerial = '0') then
+                                estado <= s3;
+                                estadoDepuracao <= "011";
+                            else
+                                estado <= s4;
+                                estadoDepuracao <= "100";
+                            end if ;
                         end if;
                     when s3 =>
-                        if (prontoConta8 = '1') then
-                            estado <= s2;
-                            estadoDepuracao <= "010";
-                        end if;
+                        estado <= s5;
+                        estadoDepuracao <= "101";
                     when s4 =>
-                        if (recebeDado = '1') then
-                            estado <= s0;
-                            estadoDepuracao <= "000";
-                        end if;
+                        estado <= s5;
+                        estadoDepuracao <= "101";
+                    when s5 =>
+                        if fimConta3 then
+                            if (bitSerial = '1') then
+                                estado <= s6;
+                                estadoDepuracao <= "101";
+                            else
+                                estado <= s2;
+                                estadoDepuracao <= "010";
+                            end if ;
+                        end if ;
                 end case;
-            elsif (serialStartBit = '0') then
-                estado <= s1;
+            elsif (bitSerial = '0') then
+                estado <= s2;
+                estadoDepuracao <= "010"
             end if;
         end if;
     end process;
@@ -71,35 +80,54 @@ begin
     begin
         case estado is
             when s0 =>
-                pronto <= '1';
-                zera <= '1';
-                registra <= '0';
-                conta <= '0';
-                temDado <= '0';
+                mostraDadoDisplay <= '0';
+                conta4 <= '0';
+                conta3 <= '0';
+                registraPonto <= '0';
+                registraTraco <= '0';
+                zera <= '0';
             when s1 =>
-                pronto <= '0';
+                mostraDadoDisplay <= '0';
+                conta4 <= '0';
+                conta3 <= '0';
+                registraPonto <= '0';
+                registraTraco <= '0';
                 zera <= '0';
-                registra <= '0';
-                conta <= '1';
-                temDado <= '0';
             when s2 =>
-                pronto <= '0';
+                mostraDadoDisplay <= '0';
+                conta4 <= '1';
+                conta3 <= '0';
+                registraPonto <= '0';
+                registraTraco <= '0';
                 zera <= '0';
-                registra <= '1';
-                conta <= '1';
-                temDado <= '0';
             when s3 =>
-                pronto <= '0';
+                mostraDadoDisplay <= '0';
+                conta4 <= '0';
+                conta3 <= '1';
+                registraPonto <= '0';
+                registraTraco <= '1';
                 zera <= '0';
-                registra <= '0';
-                conta <= '1';
-                temDado <= '0';
             when s4 =>
-                pronto <= '0';
+                mostraDadoDisplay <= '0';
+                conta4 <= '0';
+                conta3 <= '1';
+                registraPonto <= '1';
+                registraTraco <= '0';
                 zera <= '0';
-                registra <= '0';
-                conta <= '0';
-                temDado <= '1';
+            when s5 =>
+                mostraDadoDisplay <= '0';
+                conta4 <= '0';
+                conta3 <= '0';
+                registraPonto <= '0';
+                registraTraco <= '0';
+                zera <= '0';
+            when s6 =>
+                mostraDadoDisplay <= '1';
+                conta4 <= '0';
+                conta3 <= '0';
+                registraPonto <= '0';
+                registraTraco <= '0';
+                zera <= '1';
         end case;
     end process;
 end architecture;
